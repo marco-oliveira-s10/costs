be09aa3e87fb69ee060f939359d7e3e8bbf70cba
Marco Oliveira <marco.oliveira.s10@hotmail.com>
2026-06-30 14:16:22 -0300

    Adiciona componentes de layout, paginas e rotas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import NewProject from './components/pages/NewProject';
+import Container from './components/layout/Container';
 
 function App() {
   return (
-    <div>
-      <p>Costs</p>
-    </div>
+    <BrowserRouter>
+
+      <Link to="/">Home</Link>
+      <Link to="/company">Empresa</Link>
+      <Link to="/contact">Contato</Link>
+      <Link to="/newproject">Novo Projeto</Link>
+
+
+      <Container customClass="min-height">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/company" element={<Company />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/newproject" element={<NewProject />} />
+        </Routes>
+      </Container>
+
+
+    </BrowserRouter>
+
   );
 }
 
